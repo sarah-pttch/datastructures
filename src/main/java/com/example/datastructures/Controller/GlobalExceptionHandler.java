c351f58d1f96b0396ea9bc7b8e43290805d01990
sarah-pttch <sarah.pttch@gmail.com>
2023-06-23 22:33:32 +0200

    added ControllerAdvice

---
+++ src/main/java/com/example/datastructures/Controller/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.example.datastructures.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // indexoutofboundsexception
+    // methodargumenttypemismatchexception
+    // MissingServletRequestParameterException
+
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String>
+    handleIndexOutOfBoundsException(){
+        return new ResponseEntity<>("There are no elements in the list. Add elements first.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String>
+    handleTypeMismatchException(){
+        return new ResponseEntity<>("Only numbers of type Integer allowed.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String>
+    handleMissingServletRequestParameterException(){
+        return new ResponseEntity<>("A number to add must be specified.", HttpStatus.BAD_REQUEST);
+    }
+
+}
